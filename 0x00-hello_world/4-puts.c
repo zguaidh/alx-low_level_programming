ab39a1c61e1e0c0d130f9e784473a6667e414197
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-15 21:07:25 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio/h>
+/*
+ * puts function, printing a string using the function puts
+ */
+int main(void)
+{
+	puts(""Programming is like building a multilingual puzzle")
+	getchar()
+	return(0)
+}
