504cdf91d7eda584bd3ff47e340aaa478a80de6f
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-17 04:51:32 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  printing all single digit numbers of base 10 starting from 0
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
