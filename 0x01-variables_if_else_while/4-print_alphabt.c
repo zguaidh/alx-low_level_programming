c6631c20d33ee8d70849ca9f591b339972213011
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-17 04:07:24 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  printing the alphabet in lowercase
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+	char alph;
+
+	while (alph != 'q' || alph != 'e')
+	{
+
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+		putchar(alph);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
