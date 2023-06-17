5c904bb57561dacaab610ff452e8f3d1d1a9daa3
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-17 02:43:24 -0700

    task 2 mod  2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  printing the alphabet in lowercase
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
+
