fc378be1f82ee5211d5f0e56a1a5baece44a359c
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-21 12:31:25 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,13 @@
  */
 int print_last_digit(int i)
 {
-	int last_digit = i % 10;
+	int last_digit;
 
+	last_digit = i % 10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchar(last_digit + '0');
 	return (last_digit);
-	_putchar('\n');
 }
