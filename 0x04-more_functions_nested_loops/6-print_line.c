b7bb6b7b008bb35fdbcb099e2a6a68e6d4539b9e
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-22 21:28:40 -0700

    task 6 agin

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,15 @@
  */
 void print_line(int n)
 {
-	int i;
-
-	if (n <= '0')
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = '1'; i <= n; i++)
+		int i;
+
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,2 +1,21 @@
 #include "main.h"
+/**
+ *print_diagonal - draws a diagonal line
+ *
+ *Description: function that draws a diagonal line on the terminal.
+ *
+ *Return: no return
+ */
+void print_diagonal(int n)
+{
+	int x, y;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (j = 1; j <= n; j	
