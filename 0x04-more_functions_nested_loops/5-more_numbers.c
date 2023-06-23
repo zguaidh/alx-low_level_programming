f326948812e8f17c4e0d479701882f807d5fb03c
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-23 13:38:31 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,18 +11,15 @@ void more_numbers(void)
 	int i;
 	int count;
 
-	for (count = '0'; count <= '9'; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
-				_putchar(i);
+				_putchar((i / 10) + '0');
 			}
-			else
-			{
-				_putchar(i % 10 + '0');
-			}
+		_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
