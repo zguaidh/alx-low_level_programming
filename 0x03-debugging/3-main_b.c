f51b6bb5a098d9924e8c85e7b0c4f0c46bbbd83c
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-23 15:53:01 -0700

    task 3

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,5 @@
+carrie@ubuntu:/debugging$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 3-convert_day.c 3-print_remaining_days.c 3-main_b.c -o 3-main_b 
+carrie@ubuntu:/debugging$ ./3-main_b
+Date: 02/29/2000
+Invalid date: 02/29/2000
+carrie@ubuntu:/debugging$
