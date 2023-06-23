67a245df96b151f875d5c683a8dd9f6b279c5b73
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-23 05:53:24 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	/**
+	 * i increment(i++) is missing
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
