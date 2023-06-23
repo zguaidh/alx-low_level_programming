3ba0dc281408aaa41b8dc6178dd76d2c0f190eb8
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-22 19:24:57 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - multiplication of two int
+ *
+ *Description: function that multiplies two integers.
+ *@a integer to be checked
+ *@b integer to be checked
+ *
+ *Return: 0
+ */
+ int mul(int a, int b)
+{
+	if 
