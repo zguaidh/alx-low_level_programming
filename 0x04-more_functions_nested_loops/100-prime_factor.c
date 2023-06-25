5eadf8e5a592dfb44fe68b3ab04c59deda41c952
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-25 13:26:22 -0700

    task 101

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: program that finds and prints the largest prime factor of the number 612852475143
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
