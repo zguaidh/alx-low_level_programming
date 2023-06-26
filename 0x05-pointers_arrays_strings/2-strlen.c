fdaebed38954f0eb5c54e34d40664cec3770910f
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-26 13:55:26 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strlen - function that returns the length of a string.
+ *@s: the string that wer going to calculate its length
+ *Return: length
+ */
+int _strlen(char *s)
+{
+	for (s = 0; s[i] != '\0'; i++)
+	return (i);
+}
