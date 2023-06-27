e6fe9a2c59eda7aaf4113706cf275f48f818cb12
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-26 18:05:01 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,5 +6,16 @@
  */
 void puts_half(char *str)
 {
+	int i;
 
+	for (i = 0; str[i] != 0; i++)
+		;
+
+	i++;
+
+	for (i = i / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
