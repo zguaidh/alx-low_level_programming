64d41824fdd29da089643668a366dce543006099
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-29 16:48:31 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,20 +8,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int diff = ((int)*s1 - 48) - ((int)*s2 - 48);
+	int diff = 0;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (*s1)
 	{
-		if (s1[i] < s2[i])
+		if (*s1 != *s2)
 		{
-			return (diff);
+			diff = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
 		}
-		else if (s1[i] > s2[i])
-		{
-			return (diff);
-		}
-		i++;
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (diff);
 }
