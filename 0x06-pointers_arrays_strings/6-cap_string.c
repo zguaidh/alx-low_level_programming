9d7f7ee1fbceecd9124be1741906dbbabb7c5a22
habiba zguaid <zguaidhabiba@gmail.com>
2023-06-29 18:43:27 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,54 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ *lowercase - deterine whether the char is lower
+ *@x: character
+ * Return: 1 if true, 0 if false
+ */
+int lowercase(char x)
+{
+	return (x >= 'a' && x <= 'z');
+}
+
+/**
+ *Separators - deterine whether the char is Separator
+ *@x: character
+ * Return: 1 if true, 0 if false
+ */
+int Separators(char x)
+{
+	int i;
+	char sep[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (x == sep[i])
+			return (1);
+	return (0);
+}
+
+
 /**
  **cap_string -  function that capitalizes all words of a string.
  *@str : the string
  *Return: str
  */
+
 char *cap_string(char *str)
 {
-	int i = 0;
+	int issep = 1;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (Separators(*str))
+			issep = 1;
+		else if (lowercase(*str) && issep)
 		{
-			str[i] = str[i] - 32;
+			*str -= 32;
+			issep = 0;
 		}
 		else
-		{
-			str[i] = str[i];
-		}
+			issep = 0;
+		str++;
 	}
 	return (str);
 }
