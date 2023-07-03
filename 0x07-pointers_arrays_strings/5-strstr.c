d919e2d91c1efed49418602fa0871e1d4cea2801
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-03 16:51:38 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *_strstr - function that locates a substring.
+ *@haystack: the string
+ *@needle: the string to look for
+ *
+ *Return: pointer to the beginning of the located substring, or NULL
+ */
+
