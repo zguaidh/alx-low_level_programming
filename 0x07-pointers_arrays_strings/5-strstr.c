49085ac6f5502e53803ae483df3cf0d5fe3f740d
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-03 17:56:22 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,4 +6,20 @@
  *
  *Return: pointer to the beginning of the located substring, or NULL
  */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *ptr1 = haystack;
+		char *ptr2 = needle;
 
+		while (*ptr1 == *ptr2 && *ptr2 != '\0')
+		{
+			ptr1++;
+			ptr2++;
+		}
+		if (*ptr2 == '\0')
+			return (haystack);
+	}
+	return ('\0');
+}
