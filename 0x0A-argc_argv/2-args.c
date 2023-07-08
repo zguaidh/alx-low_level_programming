fa1b2ddfa34fde51df75fa152789ead028434707
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-08 16:54:18 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - entry point of a program that prints all arguments it receives
+ *@argc: number of argmnts
+ *@argv: the array
+ *
+ *Return:0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
