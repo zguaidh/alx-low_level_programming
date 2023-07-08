4012d13beb245a9f4d22f48c1ce36aceeed6c058
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-08 16:50:19 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry point
+ *@argc: the number of arguments
+ *@argv: the array of arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
