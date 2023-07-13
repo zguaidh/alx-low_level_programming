2dd9704efec2988d0432e779855a54615e882031
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-13 16:23:35 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,6 +30,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == 0)
