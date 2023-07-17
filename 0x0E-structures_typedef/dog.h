992bf43244b05eed328eaf975a131f728bba009e
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-17 14:11:34 -0700

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - dog's information
+ *@name: the name
+ *@age: the age
+ *@owner: the owner
+ *
+ * Description: a struct that provides the dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG */
