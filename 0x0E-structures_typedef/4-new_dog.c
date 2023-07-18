d05f85a01890e4cb2f7ce69c6214f2cb38e8cbc8
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-18 05:43:18 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+
 	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
@@ -22,17 +27,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(owner);
 		return (NULL);
 	}
-	else if (name == NULL || age == 0 || owner == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
 	else
 	{
 		new_dog->name = strdup(name);
 		new_dog->age = age;
 		new_dog->owner = strdup(owner);
-
-		return (new_dog);
 	}
+	if (name == NULL)
+	{
+		free(new_dog);
+		free(new_dog->name);
+	}
+	if (owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->owner);
+	}
+	return (new_dog);
+
 }
