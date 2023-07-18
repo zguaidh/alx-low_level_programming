1387da5d7ae6a8d0e1ae21e36cc5a6cde53a818d
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-17 18:54:00 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,13 @@ void print_dog(struct dog *d)
 
 	if (d == NULL)
 	{
-		;
+		return;
 	}
-	else if (d)
+	else
 	{
 		if (d->name == NULL)
 		{
 			printf("Name: (nil)\n");
-		}
-		else if (d->age == 0)
-		{
-			printf("Age: (nil)\n");
 		}
 		else if (d->owner == NULL)
 		{
@@ -30,7 +26,7 @@ void print_dog(struct dog *d)
 		else
 		{
 			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
+			printf("Age: %1f\n", d->age);
 			printf("Owner: %s\n", d->owner);
 		}
 	}
