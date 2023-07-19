59d9a9a4b19001c04338f6a12a20ecbc94b17fb9
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-19 09:20:01 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *print_name - function that prints a name
+ *@name: the name to be printed
+ *@f:pointer to a function
+ *
+ *Return: no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
