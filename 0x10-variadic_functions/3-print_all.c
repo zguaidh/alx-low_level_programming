2c9d0bf11fd7e7c1190a9334f973da3716933373
habiba zguaid <zguaidhabiba@gmail.com>
2023-07-27 03:06:29 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,66 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
+ *printStr - prints a string
+ *@args: list of args
+ *Return: int
+ */
+int printStr(va_list args)
+{
+	char *str;
+	
+	str = va_arg(args, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+	printf("%s", str);
+	}
+	return (0);
+}
+/**
+ *printChar - prints a character
+ *@args: list of args
+ *Return: int
+ */
+int printChar(va_list args)
+{
+	char ch;
+	
+	ch = va_arg(args, int);
+	putchar(ch);
+	return (0);
+}
+/**
+ * printInt - prints number
+ * @args: args
+ * Return: int
+ */
+int printInt(va_list args)
+{
+	int I;
+
+	I = va_arg(args, int);
+	printf("%d", I);
+	return(0);
+}
+/**
+ * printFloat - prints float numbers
+ * @args: args
+ * Return: int
+ */
+int printFloat(va_list args)
+{
+	float F;
+
+	F = va_arg(args, double);
+	printf("%f", F);
+	return (0);
+}
+
+/**
  *print_all - funtion that prints anything
  *@format: is the list of types of arguments
  *
@@ -9,48 +69,39 @@
  */
 void print_all(const char * const format, ...)
 {
+	int j;
 	va_list args;
-	char ch;
-	int I, i;
-	float F; 
-	char *S;
 
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (j = 0; format[j] != '\0'; j++)
 	{
-		if (format[i] == 'c')
+		specifier_t sp[] =
 		{
-                        ch = va_arg(args,int);
-                        putchar(ch);
-			printf(", ");
-                }
-		else if (format[i] == 'i')
+			{'c', printChar},
+			{'i', printInt},
+			{'f', printFloat},
+			{'s', printStr},
+		};
+		unsigned int i = 0;
+		int True = 0;
+
+		for (i = 0; i < sizeof(sp)/sizeof(sp[0]); i++)
 		{
-			I = va_arg(args, int);
-			printf("%d", I);
-			printf(", ");
-		}
-		else if (format[i] == 'f')
-		{
-			F = va_arg(args, double);
-			printf("%f", F);
-			printf(", ");
-		}
-		else if (format[i] == 's')
-		{
-			S = va_arg(args, char*);
-			if (S == NULL)
+			if (*format == sp[i].format)
 			{
-				printf("(nil)");
+				sp[i].funct(args);
+				printf(", ");
+				True = 1;
+				break;
 			}
-			else
-			{
-				printf("%s", S);
-			}
-		
-	
+		}
+		if (!True)
+		{
+			;
 		}
 	}
+
+
 	va_end(args);
 	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,24 @@
 #ifndef HEADER_FILE_H
 #define HEADER_FILE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int printChar(va_list args);
+int printStr(va_list args);
+int printInt(va_list args);
+int printFloat(va_list args);
+typedef struct
+	{
+		char format;
+		int (*funct)(va_list);
+	} specifier_t;
 
 #endif
