93d3d9bb367620aa025f922dba80ae6963ad61a6
habiba zguaid <zguaidhabiba@gmail.com>
2023-08-02 07:02:43 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,15 @@
 #include "lists.h"
+
 /**
  *listint_len - returns the number of elements
  *@h: the header
  *Return: the number of elements
  */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
+
 	while (h)
 	{
 		h = h->next;
