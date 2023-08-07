37ef2fda09cb55c9fd4a8e1a1b478db969bf03df
habiba zguaid <zguaidhabiba@gmail.com>
2023-08-07 14:59:49 -0700

    task 0 betty fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,13 +12,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int file;
 	ssize_t r;
 	char My_arr[1024];
-	
+
 	if (!filename)
 		return (0);
 	if (!letters)
 		return (0);
 	file = open(filename, O_RDONLY);
-	if(file == -1)
+	if (file == -1)
 		return (0);
 	r = read(file, &My_arr[0], letters);
 	r = write(STDOUT_FILENO, &My_arr[0], r);
