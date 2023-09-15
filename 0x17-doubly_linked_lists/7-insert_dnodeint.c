4646857d7fcf04ba6b57d54f8a31c45c120c51ef
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-15 08:54:42 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,10 +28,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 	}
 	current = *h;
-	if (len == 0)
-		return (NULL);
 	if (idx >= len)
+	{
+		free(newNode);
 		return (NULL);
+	}
 	if (idx == 0)
 	{
 		newNode->next = *h;
@@ -39,11 +40,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = newNode;
 		return (newNode);
 	}
-	for (i = 0; i < idx ; i++)
+	for (i = 0; i < idx - 1 ; i++)
 		current = current->next;
-	newNode->prev = current->prev;
-	current->prev->next = newNode;
-	newNode->next = current;
-	current->prev = newNode;
+	newNode->prev = current;
+	newNode->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = newNode;
+	current->next = newNode;
 	return (newNode);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,5 +29,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+size_t _print_dlistint_backward(const dlistint_t *h);
+
+dlistint_t *_create_dlist(unsigned int n, ...);
+void _free_dlistint(dlistint_t *list);
 
 #endif
