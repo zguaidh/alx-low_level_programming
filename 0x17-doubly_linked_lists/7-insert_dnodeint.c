a7fe29ab79faffc6fe824cca39271f44c58d70fe
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-15 08:32:44 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,12 +1,9 @@
 #include "lists.h"
-
 /**
  *insert_dnodeint_at_index -  inserts a new node at a given position
- *
  *@h: head
  *@idx: the given index where the new node should be added.
  *@n: the data of the node
- *
  *Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -20,12 +17,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newNode->n = n;
 	newNode->next = NULL;
 	newNode->prev = NULL;
-
 	if (*h == NULL && idx == 0)
 	{
 		*h = newNode;
 		return (newNode);
-
 	}
 	while (current != NULL)
 	{
@@ -37,16 +32,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx >= len)
 		return (NULL);
-
-	for (i = 0; i < idx; i++)
+	if (idx == 0)
 	{
-		current = current->next;
+		newNode->next = *h;
+		(*h)->prev = newNode;
+		*h = newNode;
+		return (newNode);
 	}
+	for (i = 0; i < idx ; i++)
+		current = current->next;
 	newNode->prev = current->prev;
 	current->prev->next = newNode;
 	newNode->next = current;
 	current->prev = newNode;
-
 	return (newNode);
-
 }
